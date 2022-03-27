65ab23bfd9221526d5306c61d2b23faab3186dde
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-03-27 17:12:54 +0100

    Create Exercise8.cs

---
+++ Array_Exercises/Exercise8.cs
@@ -0,0 +1,52 @@
+/*Selection Sort*/
+
+public class Program
+{
+	public static void Main()
+	{
+        Console.WriteLine("SELECTION SORT");
+        Console.WriteLine("--------------");
+
+        int temp = 0;
+
+        Console.Write("Enter the length of the array: ");
+        int len = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[len];
+
+        Console.Write("Enter the value(s) of the array: ");
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write("\nMatrix {0} = ", i);
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("The initial array is: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            int minimum = i;
+            
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j] == arr[minimum])
+                {
+                    minimum = j;
+                }
+            }
+
+            temp = arr[i];
+            arr[i] = arr[minimum];
+            arr[minimum] = temp;
+        }
+
+        Console.Write("The sorted array is: ");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+    }
+}
